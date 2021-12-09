a4c63fdfcd303eb7a6811221b15c46aa6cfd69c6
capjamesg <jamesg@jamesg.blog>
2021-12-09 08:02:28 +0000

    add sys path code so autofunction wil work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ project = 'IndieWeb Utils'
 copyright = 'capjamesg 2021'
 author = 'capjamesg'
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..') + "src/")
 
 release = '0.1'
 version = '0.1.0'
