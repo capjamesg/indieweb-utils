265c4f3966991210f1ef0315fc5e6a92590346ed
James Van Dyne <james@sugoisoft.com>
2022-01-04 08:39:49 +0900

    Adopt parameterize for canonicalize url test

--- tests/test_indieweb_utils.py
+++ tests/test_indieweb_utils.py
@@ -1,24 +1,32 @@
 # Partial test coverage is implemented for various functions in the library
 # Assistance is requested to achieve full test coverage
+import pytest
 
 from indieweb_utils import *
-import mf2py
 
 
-class TestCanonicalizeURL:
-    def test_canonicalize_url(self):
-        assert canonicalize_url('http://www.example.com/', "example.com") == 'http://www.example.com/'
-        assert canonicalize_url('example.com/', "example.com") == 'https://example.com/'
-        assert canonicalize_url('http://www.example.com:80/', "example.com") == 'http://www.example.com/'
-        assert canonicalize_url('example.com/../', "example.com") == 'https://example.com/'
-        assert canonicalize_url('example.com/./image.png', "example.com") == 'https://example.com/image.png'
-        assert canonicalize_url('example.com/../image.png', "example.com") == 'https://example.com/image.png'
+@pytest.mark.parametrize(
+    "url, expected",
+    [
+        ("http://www.example.com/", "http://www.example.com/"),
+        ("example.com/", "https://example.com/"),
+        ("http://www.example.com:80/", "http://www.example.com/"),
+        ("example.com/../", "https://example.com/"),
+        ("example.com/./image.png", "https://example.com/image.png"),
+        ("example.com/../image.png", "https://example.com/image.png"),
+    ],
+)
+def test_canonicalize_url(url, expected):
+    domain = "example.com"
+    assert canonicalize_url(url=url, domain=domain) == expected
 
 
 class TestWebmentionEndpointDiscovery:
     def test_webmention_endpoint_discovery(self):
         # TODO: Mock the request so unit tests are making network requests
-        endpoints = discover_webmention_endpoint("https://jamesg.blog/2021/12/06/advent-of-bloggers-6/")
+        endpoints = discover_webmention_endpoint(
+            "https://jamesg.blog/2021/12/06/advent-of-bloggers-6/"
+        )
         assert len(endpoints) > 0
         assert endpoints[0] == "https://webmention.jamesg.blog/endpoint"
 
@@ -30,6 +38,6 @@ class TestPostTypeDiscovery:
 
         parsed = mf2py.parse(url=post_url)
 
-        h_entry = [item for item in parsed["items"] if item.get('type') == ['h-entry']]
+        h_entry = [item for item in parsed["items"] if item.get("type") == ["h-entry"]]
 
         assert get_post_type(h_entry[0]) == "article"
