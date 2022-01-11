3d5b0679aba3a6701e2641abf8b0202608fa7f67
James Van Dyne <james@sugoisoft.com>
2022-01-12 07:05:03 +0900

    Extract indieauth module

--- tests/test_indieweb_utils.py
+++ tests/test_indieweb_utils.py
@@ -1,6 +1,7 @@
 # Partial test coverage is implemented for various functions in the library
 # Assistance is requested to achieve full test coverage
 import pytest
+import mf2py
 
 from indieweb_utils import *
 
@@ -46,7 +47,7 @@ class TestWebPageFeedDiscovery:
         feeds = discover_web_page_feeds("https://jamesg.blog/")
 
         assumed_feeds = [
-            "https://jamesg.blog/", # h-feed
+            "https://jamesg.blog/",  # h-feed
             "https://jamesg.blog/feeds/posts.xml",
             "https://jamesg.blog/feeds/posts.jf2",
             "https://jamesg.blog/feeds/posts.json",
