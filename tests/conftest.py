018991150b37ecee47e2d4d308c5d00956bb6757
James Van Dyne <james@sugoisoft.com>
2022-01-16 10:26:57 +0900

    Add index fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,14 @@ def article() -> str:
 @pytest.fixture
 def article_url() -> str:
     return "https://jamesg.blog/2021/12/06/advent-of-bloggers-6/"
+
+
+@pytest.fixture
+def index() -> str:
+    with open("tests/fixtures/index.html") as f:
+        return f.read()
+
+
+@pytest.fixture
+def index_url() -> str:
+    return "https://jamesg.blog/"
