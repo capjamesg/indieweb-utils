285480489c56c7e8cd79bebc958ed41cb19da23c
capjamesg <jamesg@jamesg.blog>
2022-01-31 10:40:31 +0000

    add example code snippet

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode"
+    "sphinx.ext.viewcode",
 ]
 
 intersphinx_mapping = {
