8ea31d5ed2f954ae8130570c5a6f0a1be4875a52
capjamesg <jamesg@jamesg.blog>
2022-09-22 14:25:54 +0100

    change documentation theme to alabaster

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
